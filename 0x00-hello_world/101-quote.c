39e9e96f7caae3d35089cd3da6c1b3ae48c55cd5
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-07 21:42:43 -0700

    unix is simple, the trick is understanding the simplicity_edit-6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 {
 	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	syscall(SYS_write, 1, x, (sizeof(x)-1));
+	syscall(SYS_write, 1, x, (sizeof(x) - 1));
 	return (1);
 }
