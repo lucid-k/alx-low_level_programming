3048a99f860cc6b4af6b32ed10c29f22448d5947
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-07 21:28:39 -0700

    unix is simple, the trick is understanding the simplicity_edit-1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+
 /**
  * main - runs function
  *
- * return - 1(standard error)
- *
+ * Return: Always 1 (standard error)
  */
-
 int main(void)
 {
-	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	syscall(SYS_write, 1, x, sizeof(x));
 	return (1);
-}	
+}
