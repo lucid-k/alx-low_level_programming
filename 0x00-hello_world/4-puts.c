c6bf21e01f1668e8787bb13f7d5162f3b9a5f3a3
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-07 19:25:57 -0700

    hello,pritf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of
- * art,"
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
