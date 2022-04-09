287dd8b131c2411009308fb82d051300315a84e7
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-08 17:41:40 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: print all possible 
+ * combinations of 
+ * single digit numbers without char
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
