a2a19cec866f9594506ea43065b95d4598b52d3d
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-08 18:25:22 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - print number combinations
+ *
+ * Description: print all possible
+ * combinations of
+ * three digits numbers without repetition
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int i = 48, j = 48, k = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			k = j + 1;
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55 || j < 56 || k < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
