f14892e593018f6047eddab000a716dcfb1febfb
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-13 19:02:31 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function definition
+ * Definition - add 2 numbers
+ * Return: result of addition.
+ */
+int add(int a, int b)
+{
+	int sum = 0;
+
+	sum = a + b;
+	return (sum);
+}
