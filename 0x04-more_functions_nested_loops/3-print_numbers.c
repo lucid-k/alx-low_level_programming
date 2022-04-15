ebd7f9757de36e7c270b99c8b14095d444f53553
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-14 19:45:24 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints 0-9 numbers
+ *
+ */
+
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
