cccb28b109bdcc60f2daa7a2b3b49fa59eea845a
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-14 21:19:00 -0700

    F U ALX

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,35 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints numbers 1 to 100
+ *main - solution to fizz buzz problem
  *
- * Return: always 0
+ *Return: Always 0
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+
+	printf("%d ", i);
+
+	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
-
-		printf("FizzBuzz ");
-
-		} else if (i % 5 == 0)
-		{
-
-			printf("Buzz ");
-
-		} else if (i % 3 == 0)
-		{
-
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
 			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
 
-		} else
-		{
-
-		printf("%d ", i);
-
-		}
+		if (i < 100)
+			putchar(' ');
+		else
+			putchar(' ');
 	}
-	printf("\n");
 	return (0);
 }
