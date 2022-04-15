ff8675a0d7ca6b8c48e8391ca81f772388b3d499
Anthony Maiyo <maiyokimmant@gmail.com>
2022-04-14 21:07:39 -0700

    hotfix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,28 +13,29 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; 1 <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 
-		printf("Fizz ");
+		printf("FizzBuzz ");
 
 		} else if (i % 5 == 0)
 		{
 
 			printf("Buzz ");
 
-		} else if (i % 3 == 0 && i % 5 == 0)
+		} else if (i % 3 == 0)
 		{
 
-			printf("FizzBuzz ");
+			printf("Fizz ");
 
 		} else
 		{
 
 		printf("%d ", i);
 
+		}
 	}
 	return (0);
 }
